f9cbaf7ac809d984b14784a23029fcddc90d99ea
Raqeeb Anjum <rraqeebanjum04@gmail.com>
2024-03-11 17:13:34 -0500

    Initial Commit

---
+++ MetaClean/MetaCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaCleanApp.swift
+//  MetaClean
+//
+//  Created by Raqeeb Anjum on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MetaCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
